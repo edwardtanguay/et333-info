82788e9335b9b99b0afdcf02c93979c102641da6
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-25 19:37:52 +0200

    added list

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <h1>Info Site</h1>
   <p>This is our site.</p>
+  <ul>
+    <li>first thing</li>
+    <li>second thing</li>
+    <li>third thing</li>
+  </ul>
 `
